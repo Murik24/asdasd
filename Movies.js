927808d18417eb5e786cd8cb9b2510969cdc780d
Murik24 <mganbarov9@gmail.com>
2023-11-10 18:51:22 +0400

    updated

--- Movies.js
+++ Movies.js
@@ -9,25 +9,9 @@ function fillHtml(data){
     <div class="card-body">
         <h5 class="card-title">${element.name}</h5>
         <p class="card-text">${element.runtime}</p>
-        <a id="${element.id}" href="./details.html" class="btn btn-primary">Check</a>
+        <a id="${element.id}" href="./details.html?id=${element.id}" class="btn btn-primary">Check</a>
     </div>
     </div>`
-        let buttons = document.querySelectorAll(".btn-primary")
-        buttons.forEach((button)=>(button.addEventListener("click",function(){
-            alert(this.id) 
-            fetch(`https://api.tvmaze.com/shows/${this.id}`)
-            .then(response => response.json())
-            .then((data) => {
-                row1.innerHTML = `<div class="card col-3" style="width: 18rem;">
-                <img src="${data.image.medium}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${data.name}</h5>
-                    <p class="card-text">${data.runtime}</p>
-                </div>
-                </div>`
-            })
-        }
-        )))
     });
 }
 
--- details.js
+++ details.js
@@ -1,17 +1,16 @@
+let detail_page = document.querySelector(".details");
 let id = new URLSearchParams(location.search).get("id")
-let buttons = document.querySelector(".btn-primary")
-        buttons.forEach((button)=>(button.addEventListener("click",function(){
-            alert(this.id) 
-            fetch(`https://api.tvmaze.com/shows/${this.id}`)
-            .then(response => response.json())
-            .then((data) => {
-                row1.innerHTML = `<div class="card col-3" style="width: 18rem;">
-                <img src="${data.image.medium}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${data.name}</h5>
-                    <p class="card-text">${data.runtime}</p>
-                </div>
-                </div>`
-            })
-        }
-        )))
+
+axios.get(`https://api.tvmaze.com/shows/${id}`)
+.then((result) => {
+    let movie = result.data
+    detail_page.innerHTML = `<div class="card col-3" style="width: 18rem;">
+    <img src="${movie.image.medium}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${movie.name}</h5>
+        <p class="card-text">${movie.runtime}</p>
+        <a href="./main.html" class="btn btn-primary">Back</a>
+    </div>
+    </div>
+    <p class="description">${movie.summary}</p>`
+})
